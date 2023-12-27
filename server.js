a6e847e7e059d63d8bd715330666da4ccbec1db6
stanxy1 <153308152+stanxy1@users.noreply.github.com>
2023-12-27 05:11:49 +0000

    Played around with multiple commands and node modules. Planning on moving on to more compleex concepts

--- server.js
+++ server.js
@@ -1,5 +1,18 @@
-let http = require('http')
-http.createServer(function (req, res) => {
-  res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Hello World!');
+var http = require('http');
+var url = require('url');
+var fs = require('fs');
+
+http.createServer(function (req, res) {
+  var q = url.parse(req.url, true);
+  var filename = "." + q.pathname;
+  fs.readFile(filename, function(err, data) {
+    if (err) {
+      res.writeHead(404, {'Content-Type': 'text/html'});
+      return res.end("404 Not Found");
+    }  
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    res.write(data);
+    return res.end();
+  });
 }).listen(8080);
+
